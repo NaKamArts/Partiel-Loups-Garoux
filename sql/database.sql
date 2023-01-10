20f25963857522cc5414e1322a993616e7dc3d87
Evans <71824258+MisterEvans78@users.noreply.github.com>
2023-01-10 15:47:12 +0100

    deplacement fichier sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,20 +1,20 @@
 CREATE TABLE IF NOT EXISTS Carte(
-   carte_id INT,
-   nom VARCHAR(255),
+   carte_id INT NOT NULL AUTO_INCREMENT,
+   nom VARCHAR(255) NOT NULL,
    image VARCHAR(255),
    description TEXT,
    PRIMARY KEY(carte_id)
 );
 
 CREATE TABLE IF NOT EXISTS Partie(
-   partie_id INT,
+   partie_id INT NOT NULL AUTO_INCREMENT,
    nbNuit INT,
    PRIMARY KEY(partie_id)
 );
 
 CREATE TABLE IF NOT EXISTS Joueur(
-   joueur_id INT,
-   pseudo VARCHAR(255),
+   joueur_id INT NOT NULL AUTO_INCREMENT,
+   pseudo VARCHAR(255) NOT NULL,
    estVivant BIT,
    estMaire BIT,
    estAmoureux BIT,
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS Joueur(
 );
 
 CREATE TABLE IF NOT EXISTS Vote(
-   vote_id INT,
+   vote_id INT NOT NULL AUTO_INCREMENT,
    joueur_id INT NOT NULL,
    partie_id INT NOT NULL,
    joueur_vote INT NOT NULL,
